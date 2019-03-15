6fe365a2c9722ac94668ad263661ac70917d9a81
Paul Siedler <paul.siedler@netresearch.de>
2019-03-15 20:44:53 +0100

    INGCOTWO-254: add doc blocks, for class and methods, add license file, add file header

--- Controller/CsrfAware/Action.php
+++ Controller/CsrfAware/Action.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * See LICENSE.md for license details.
+ */
 
 namespace Netresearch\Compatibility\Controller\CsrfAware;
 
@@ -6,20 +9,36 @@ use Magento\Framework\App\Action\Action as CoreAction;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Controller\ResultInterface;
 
+/**
+ * Class Action for providing forward compatibility for controllers that need to implement
+ * \Magento\Framework\App\CsrfAwareActionInterface following Magento's 2.3 release
+ *
+ * @author Paul Siedler <paul.siedler@netresearch.de>
+ * @link http://www.netresearch.de/
+ */
 abstract class Action extends CoreAction
 {
+    /**
+     * {@inheritdoc}
+     */
     abstract public function execute();
 
     /**
+     * Generates the response object to use for creating a \Magento\Framework\App\Request\InvalidRequestException
+     * instance
+     *
      * @param RequestInterface $request
      * @return ResultInterface
      */
     abstract protected function getCsrfExceptionResponse(RequestInterface $request);
 
     /**
+     * Proxy function for \Magento\Framework\App\CsrfAwareActionInterface::validateForCsrf
+     *
+     * Should return true, if the request is valid, otherwise false.
+     *
      * @param RequestInterface $request
      * @return bool
      */
     abstract protected function proxyValidateForCsrf(RequestInterface $request);
-
 }
